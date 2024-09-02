46f134a55a530eea604c4364e8a352369fd8ae0a
LucasLagassie <lucas.lagassie@oclock.school>
2024-09-02 10:22:05 +0200

    server is created

---
+++ index.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { router } from "./src/routers/index.js";
+
+const port = process.env.PORT || 3000;
+
+export const app = express();
+
+app.use(cors({origin: process.env.ALLOWED_DOMAINS}));
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
