4b2fe622e0b4a3a1a8a4d5d4c50e996700b4d6be
Leila-tabti <leila.tabti@oclock.school>
2024-09-03 11:12:43 +0200

    controller-recettes

---
+++ src/controllers/RecipeController.js
@@ -0,0 +1,65 @@
+import { Recipe, RecipeHasIngredient, RecipeCategory, RecipeHasCategory, Ingredient, MovieAndSerie} from "../models";
+import { HTTPError } from "../errors/httpError.js";
+
+export const getAll = async (req, res) => {
+    const recipes = await Recipe.findAll({
+        include: [
+            {
+                association: "recipeHasIngredients",
+                through: { attributes: []},
+            },
+            {
+                association: "recipeHasCategories",
+                through: { attributes: []},
+            },
+        ],
+    });
+    return res.json(recipes);
+
+}
+
+export const getOne = async (req, res) => {
+    const recipe = await Recipe.findByPk(req.params.id,{
+      include: {
+        association: "ingredients",
+        through: { attributes: [] },
+      },
+    });
+    if(!recipe){
+      throw new HTTPError(404, "Oups! Cette scène semble manquer au scénario");
+    }
+    return res.json(recipe);
+  };
+
+  export const createOne = async (req, res) =>{
+    const recipe = await Recipe.create(req.body);
+    return res.status(201).json(recipe);
+  };
+  
+  export const modifyOne = async (req, res) => {
+    const recipe = await Recipe.findByPk(req.params.id, {
+      include: {
+        association: "ingredients",
+        through: { attributes: [] },
+      },
+    });
+  
+    if(!recipe){
+      throw new HTTPError(404, "Oups! Cette scène semble manquer au scénario");
+    }
+  
+    await recipe.update(req.body);
+    return res.json(recipe);
+  };
+
+  export const deleteOne = async (req, res) => {
+    const recipe = await Recipe.findByPk(req.params.id);
+    if(!recipe){
+      throw new HTTPError(404, "Oups! Cette scène semble manquer au scénario");
+    }
+    await recipe.destroy();
+    return res.status(204).end();
+  };
+  
+  
+  
--- src/controllers/movieAndSerieController.js
+++ src/controllers/movieAndSerieController.js
@@ -1,1 +1,13 @@
 import { MovieAndSerie } from "../models/index.js";
+import { Recipe} from "../models/index.js";
+
+export const getAll = async (req, res) => {
+    const movieAndSeries = await MovieAndSerie.findAll({
+        include: {
+            association: "recipes",
+            through: { attributes: []},
+         
+        },
+    });
+    return res.json(movieAndSeries);
+}
