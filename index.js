d5c1f1ee4f80f8302e260a028480c932b026e346
Leila-tabti <leila.tabti@oclock.school>
2024-09-06 15:28:32 +0200

    search-fuse

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import { router } from "./src/routers/index.js";
 import dotenv from "dotenv";
 import { sequelize } from "./src/models/sequelize.js";
+import { MovieAndSerie } from "./src/models/movieAndSerie.js";
+import { Recipe } from "./src/models/recipe.js";
 
 const port = process.env.PORT || 3000;
 
@@ -14,6 +16,8 @@ app.use(cors({ origin: process.env.ALLOWED_DOMAINS }));
 app.use(express.json());
 
 app.use(router);
+
+app.use(notFound);
 
 app.use(express.static("public"));
 
