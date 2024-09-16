8e95e7340f66bc9840c80afafbf399fe2e945832
LucasLagassie <lucas.lagassie@oclock.school>
2024-09-16 14:06:40 +0200

    LL - register/login refacto and schema

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { Router } from "express";
 import jsonwebtoken from "jsonwebtoken"; // Import JWT library for generating tokens
-import login from "../controllers/loginController.js";
-import register from "../controllers/registerController.js";
 import { router as userRouter } from "./user.js";
 import { router as moviesSeriesRouter } from "./moviesSeries.js";
 import { router as recipeRouter } from "./recipe.js";
 import { router as adminRouter } from "./admin.js";
 import { router as ingredientRouter } from "./ingredient.js";
+import { router as loginRouter } from "./login.js";
+import { router as registerRouter } from "./register.js";
 import { cookiejwtAuth } from "../middlewares/cookiejwtAuth.js";
 
 import { errorHandler } from "../middlewares/errorHandler.js";
@@ -17,8 +17,8 @@ import { notFound } from "../middlewares/notFound.js";
 
 export const router = Router();
 
-router.post("/login", login);
-router.post("/register", register);
+router.use("/login", loginRouter);
+router.use("/register", registerRouter);
 router.use("/moviesSeries", moviesSeriesRouter);
 router.use("/recipes", recipeRouter);
 router.use("/ingredients", ingredientRouter);
