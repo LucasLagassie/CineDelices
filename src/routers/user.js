88810135930d8a79306a134e69b70327866c26f6
LucasLagassie <lucas.lagassie@oclock.school>
2024-09-16 11:43:13 +0200

    LL - schema route user

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,18 +1,60 @@
 import { Router } from "express";
-import { getUser, editUser, deleteUser, createRecipe, deleteRecipe, editRecipe } from "../controllers/userController.js";
 import { controllerWrapper } from "../controllers/controllerWrapper.js";
+import {
+  getUser,
+  editUser,
+  deleteUser,
+  createRecipe,
+  deleteRecipe,
+  editRecipe,
+} from "../controllers/userController.js";
+import {
+  idSchema,
+  updateUserSchema,
+  createUserSchema,
+  createRecipeSchema,
+  updateRecipeSchema,
+  createValidationMiddleWare,
+} from "../validation/schemas.js";
 
 export const router = Router();
 
 //Get user
-router.get('/:id', controllerWrapper(getUser));
+router.get(
+  "/:id",
+  createValidationMiddleWare(idSchema, "params"),
+  controllerWrapper(getUser)
+);
+
 //Edit user
-router.post('/:id', controllerWrapper(editUser));
+router.post(
+  "/:id",
+  createValidationMiddleWare(idSchema, "params"),
+  createValidationMiddleWare(createUserSchema, "body"),
+  controllerWrapper(editUser)
+);
 //Delete user
-router.delete('/:id', controllerWrapper(deleteUser));
+router.delete(
+  "/:id",
+  createValidationMiddleWare(idSchema, "params"),
+  controllerWrapper(deleteUser)
+);
 //Create recipe
-router.post('/recipes', controllerWrapper(createRecipe));
+router.post(
+  "/recipe",
+  createValidationMiddleWare(createRecipeSchema, "body"),
+  controllerWrapper(createRecipe)
+);
 //Delete recipe
-router.delete('/recipes/:id', controllerWrapper(deleteRecipe));
+router.delete(
+  "/recipe/:id",
+  createValidationMiddleWare(idSchema, "params"),
+  controllerWrapper(deleteRecipe)
+);
 //Edit recipe
-router.put('/recipes/:id', controllerWrapper(editRecipe))
+router.put(
+  "/recipe/:id",
+  createValidationMiddleWare(idSchema, "params"),
+  createValidationMiddleWare(updateRecipeSchema, "body"),
+  controllerWrapper(editRecipe)
+);
