5a8babe38cc0357fe12ff2288e1d408159d3cdc8
alexandrapasco <sachapasco@gmail.com>
2024-09-18 11:00:16 +0200

    AP-Resolt/Conflits

--- src/routers/Recipe.js
+++ src/routers/Recipe.js
@@ -6,12 +6,8 @@ import {
   createOne,
   updateOne,
   deleteOne,
-<<<<<<< HEAD
-} from "../controllers/RecipeController.js";
-=======
   getLatestRecipes,
 } from "../controllers/recipeController.js";
->>>>>>> cbfcfc0bed0131df905fa52b284bfcad323b1119
 import {
   updateRecipeSchema,
   idSchema,
